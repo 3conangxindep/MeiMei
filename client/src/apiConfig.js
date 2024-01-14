0ffdaf71b84704d3f332bcbf3ad83134e6b12c89
Le Thi Yen Nhi <nhile0084@gmail.com>
2024-01-14 17:56:20 +0900

    01/14

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,9 +1,5 @@
 // apiConfig.js
-<<<<<<< HEAD
-const API_BASE_URL = "10.200.0.144";
+const API_BASE_URL = "172.20.157.75";
 
-=======
-const API_BASE_URL = "172.16.10.63";
->>>>>>> e23cf8a4e0ccb8d3a5510a06ceb91a72646b1645
 
 export default API_BASE_URL
