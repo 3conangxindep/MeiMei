5644c4466969afe0214d8f60170aaaca6c6f4e04
rinrin011 <miraikaranoonanoko@gmail.com>
2024-01-24 12:29:06 +0900

    1/24

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { AuthProvider } from './components/AuthContext';
 import { SelectedOptionsProvider } from './components/SelectedOptionsContext';
 import LoginPage from './components/LoginPage/LoginPage';
 import SignUpPage from './components/SignUpPage/SignUpPage';
+import CardManages from './components/CardManages/CardManages';
 import Main from './components/HomePage/Main';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Switch>
             <Route path={`/main/:contact_id`} component={Main} />
             <Route path="/signUpPage" component={SignUpPage} />
+            <Route path="/cardManages" component={CardManages} />
             <Route component={LoginPage} />
           </Switch>
         </Router>
